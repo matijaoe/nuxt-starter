69564ea1aa7b3c013fe0e92f56b60c0730e70676
Matija Osrečki <github@matijao.com>
2023-12-25 23:28:33 +0100

    chore: update to nuxt 3.9.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
     vue: {
       script: {
         defineModel: true,
-        propsDestructure: true,
+        propsDestructure: false,
       },
     },
   },
